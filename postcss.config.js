1405dce892e6c418d55e510a7ee8fa3f03f8346b
Krisa <krisa@krisa.cz>
2020-04-28 12:58:00 +0200

    Paths update

--- postcss.config.js
+++ postcss.config.js
@@ -4,6 +4,7 @@ const tailwindcss = require('tailwindcss');
 const purgecss = require('@fullhuman/postcss-purgecss')({
     content: [
         './htdocs/site/templates/*.php',
+        './htdocs/site/templates/**/*.php',
         './htdocs/site/snippets/*.php',
         './htdocs/site/snippets/**/*.php',
     ],
